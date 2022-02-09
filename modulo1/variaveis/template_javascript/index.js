8e36f175fe025677be8ea095c872493b3de00bd6
vivianmeireles <vivian.meireles@icloud.com>
2022-02-08 23:40:56 -0300

    criei o arquivo index.js

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,70 @@
+// respostas dos exercicios de interpretacão
+
+// questão 1 
+// console.log (a , b) = 10 , 5
+
+// questão 2
+// console.log (a , b , c) = 10 , 10 , 10
+
+// questão 3
+//let horaTrabalho = prompt("Qual a sua carga horária diária?")
+//let valorDia = prompt("Quanto você recebe por dia?")
+//alert (`Você recebe ${horaTrabalho/valorDia} por hora`)
+
+// respostas dos exercicios de escrita de código
+
+let nomeDoUsuario 
+let idadeDoUsuario 
+console.log(typeof nomeDoUsuario)
+console.log(typeof idadeDoUsuario) 
+// undefined porque não tem nenhum valor definido 
+nomeDoUsuario = prompt ("Qual o seu nome? ")
+idadeDoUsuario = prompt (" Qual a sua idade?")
+console.log( "Olá,", nomeDoUsuario, ",você tem" , idadeDoUsuario , "anos." )
+
+
+animais = prompt ("Você gosta de animais?")
+idiomas = prompt ("Você fala mais de 3 idiomas?")
+comidas = prompt ("Você gosta de cozinhar?")
+console.log( "Você gosta de animais?", animais)
+console.log( "Você fala mais de 3 idiomas?", idiomas)
+console.log( "Você gosta de cozinhar?", comidas)
+
+let a = 10
+let b = 25
+let c = a
+console.log("O novo valor de a é", b)
+console.log("O novo valor de b é", a)
+
+// resposta do desafio
+
+
+a = prompt ("digite um número")
+b = prompt ("digite um novo número")
+let x = Number(a) + Number(b)
+let y = Number(a) * Number(b)
+console.log("o valor da soma dos números é" ,x, "e o valor da multiplicação dos números é" ,y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
