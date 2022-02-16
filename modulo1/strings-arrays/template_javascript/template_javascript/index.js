f9e9fe992dd0030b2333d3e2db74a972cb69992a
vivianmeireles <vivian.meireles@icloud.com>
2022-02-16 00:05:20 -0300

    criei o arquivo index.js

---
+++ modulo1/strings-arrays/template_javascript/template_javascript/index.js
@@ -0,0 +1,84 @@
+// Respostas dos exercícios de interpretação de código
+
+// 1. a. indefined
+//b. null 
+//c. a soma de todos os caracteres da frase é 11 
+//d. i=0, 0=3. Então, o resultádo é 3 
+//e. como i=0, 0=3, a soma de 3+1 é 4, então, ele trocou o ítem 4 pelo ítem 19 
+//f. como i=0, 0=3, a soma de 3+6 é 9
+
+
+// 2. ele troca todas as letras "A" contidas na frase por "I" e mostra o número de caracteres na frase, incluindo os espaços.
+
+
+
+// Respostas dos exercícios de escrita de código
+
+// 1.
+//let nomeUsuario = prompt("Qual o seu nome?")
+//let emailUsuario = prompt("Qual o seu e-mail?")
+
+//console.log(`O e-mail ${emailUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeUsuario}!`)
+
+
+// 2.
+
+//a. 
+//const comidasFav = ["Feijoada" , "Lasanha" , "Lamen" , "Boeuf Bourguignon" , "Chilli com carne" ]
+//console.log(comidasFav)
+
+//b.
+//console.log(`Essas são as minhas comidas preferidas: 
+//${comidasFav[0]} ,
+//${comidasFav[1]} ,
+//${comidasFav[2]} ,
+//${comidasFav[3]} e
+//${comidasFav[4]}`)
+
+//c.
+//let listaUsuario = prompt("Qual a sua comida preferida?")
+//comidasFav[1] = listaUsuario
+//console.log(comidasFav)
+
+
+
+
+//3.
+
+//a.
+
+listaDeTarefas = []
+
+//b.
+
+const primeiraTarefa = prompt("Insire Sua Primeira Tarefa")
+const segundaTarefa = prompt("Insire Sua Segunda Tarefa")
+const terceiraTarefa = prompt("Insire Sua terceira Tarefa")
+
+listaDeTarefas.push(primeiraTarefa, segundaTarefa, terceiraTarefa)
+
+//c. 
+
+console.log(`As tarefas do dia são: ${listaDeTarefas}`)
+
+//d.
+
+let tarefaRealizada = prompt("Qual a tarefa que você já realizou: 0, 1 ou 2?")
+
+//e.
+
+//listaDetarefas.splice(tarefaRealizada)
+//const removed = listaDetarefas.splice(tarefaRealizada)
+//listaDeTarefas.splice(indiceUsuario -1, 1)
+
+
+
+console.log(listaDetarefas)
+
+
+
+
+
+//let indice = prompt("Qual a tarefa já realizou? 0, 1 ou 2?")
+//listaDetarefas.splice(indice,1)
+//console.log(listaDetarefas)
